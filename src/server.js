e00fdade8d7ffd88b4dfc13e8721c8d07f22fa5d
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2025-10-15 14:08:37 +0100

    git commit -m "Deploy to Render"

--- src/server.js
+++ src/server.js
@@ -50,5 +50,5 @@ app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
-  console.log(`✅ Server running on port ${PORT}\n📘 Swagger Docs: http://localhost:${PORT}/api-docs`)
+  console.log(`✅ Server running on port ${PORT}\n📘 Swagger Docs: http://localhost:/api-docs`)
 );
